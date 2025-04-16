b6c6cf20185cc34dfad3319c9367bd1909ead67d
ray-a135 <rachelauguste36@gmail.com>
2025-04-16 13:46:49 -0700

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
 let mySprite = sprites.create(assets.image`ert`, SpriteKind.Player)
+game.showLongText("the enemy is going  to kill you run", DialogLayout.Bottom)
+let mySprite11 = sprites.create(assets.image`uio`, SpriteKind.Player)
 let mySprite2 = sprites.create(assets.image`iop`, SpriteKind.Player)
+scene.setBackgroundColor(0)
 let mySprite3 = sprites.create(assets.image`999`, SpriteKind.Enemy)
+let mySprite13 = sprites.create(img`
+    . . . . . c c c c c c c . . . . 
+    . . . . c 6 7 7 7 7 7 6 c . . . 
+    . . . c 7 c 6 6 6 6 c 7 6 c . . 
+    . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+    . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+    . . f 7 8 1 f f 1 6 7 7 7 f . . 
+    . . f 6 f 1 f f 1 f 7 7 7 f . . 
+    . . . f f 2 2 2 2 f 7 7 6 f . . 
+    . . c c f 2 2 2 2 7 7 6 f c . . 
+    . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+    c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+    f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+    f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+    . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+    . . f f c c c c c c c c . . . . 
+    `, SpriteKind.Enemy)
+scene.setBackgroundImage(assets.image`9787212rr9r8r8tjdjikjkji0j`)
 let mySprite4 = sprites.create(assets.image`0000`, SpriteKind.Enemy)
 let mySprite7 = sprites.create(img`
     . . . . . . f f f f . . . . . . 
@@ -20,23 +42,28 @@ let mySprite7 = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-let mySprite5 = sprites.create(assets.image`jj`, SpriteKind.Enemy)
+controller.moveSprite(mySprite)
+controller.player2.moveSprite(mySprite2)
+controller.player3.moveSprite(mySprite11)
+controller.player4.moveSprite(mySprite7)
+let mySprite10 = 0
+let mySprite5 = 0
 let mySprite6 = sprites.create(img`
-    ...................cc...
-    ...............cccc63c..
-    ..............c633336c..
-    ..........cc.c6cc33333c.
-    .........b55c6c55c33333c
-    .........ff5c6c5ff33333c
-    .........ff5c6c5ff6333cc
-    .........b553c355c6666cc
-    ..........b55355c333333c
-    .........cc55555bcc3333c
-    ........c5545554b55c33c.
-    ........b54b4444bb5cbb..
-    ........c455b4b5554c45b.
-    ........c555c4c555c4c5c.
-    ........c5555c5555c4c5c.
-    .........ccccccccc..ccc.
-    `, SpriteKind.Enemy)
-tiles.setCurrentTilemap(tilemap`level1`)
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+let mySprite8 = 0
