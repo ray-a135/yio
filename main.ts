15e890b7e3ac4440e6f155b1fc498f066acf6a79
ray-a135 <rachelauguste36@gmail.com>
2025-04-16 10:07:07 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
-
+let mySprite = sprites.create(assets.image`ert`, SpriteKind.Player)
+let mySprite2 = sprites.create(assets.image`iop`, SpriteKind.Player)
+let mySprite3 = sprites.create(assets.image`999`, SpriteKind.Enemy)
+let mySprite4 = sprites.create(assets.image`0000`, SpriteKind.Enemy)
+let mySprite5 = sprites.create(assets.image`jj`, SpriteKind.Player)
+let mySprite6 = sprites.create(img`
+    ...................cc...
+    ...............cccc63c..
+    ..............c633336c..
+    ..........cc.c6cc33333c.
+    .........b55c6c55c33333c
+    .........ff5c6c5ff33333c
+    .........ff5c6c5ff6333cc
+    .........b553c355c6666cc
+    ..........b55355c333333c
+    .........cc55555bcc3333c
+    ........c5545554b55c33c.
+    ........b54b4444bb5cbb..
+    ........c455b4b5554c45b.
+    ........c555c4c555c4c5c.
+    ........c5555c5555c4c5c.
+    .........ccccccccc..ccc.
+    `, SpriteKind.Enemy)
+tiles.setCurrentTilemap(tilemap`level1`)
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100002020202020202020202020202020202020101010201010101010101010102020201020102020202020202020201020102010201010101010101010101010201020102010201010101010101010102020201020102020102010102010201010202010201020101020101020102010102020102010201010201010201020201020201020102010102010102010102010202010201020201020101020101020102020102010102010201010201010201020201020101020102010102010102010202010201010201020201020201020102020102010102010102010102010102020201020101020101020101020101020202020202020202020202020202020202`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass3,sprites.builtin.forestTiles0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
